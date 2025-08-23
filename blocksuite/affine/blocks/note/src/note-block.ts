9c9d6d9ef9a5a5d75a8c94990c13d525e8b1a758
alighasami <alighassami@yahoo.com>
2025-08-23 18:16:22 +0330

    fix : jupm when open embed page

--- blocksuite/affine/blocks/note/src/note-block.ts
+++ blocksuite/affine/blocks/note/src/note-block.ts
@@ -16,8 +16,32 @@ export class NoteBlockComponent extends BlockComponent<NoteBlockModel> {
     }
   `;
 
+  override async getUpdateComplete() {
+    const result = await super.getUpdateComplete();
+    checkNotEmptyNote(this.model, this.doc);
+    /*try{
+      let  lastChild :  null | BlockModel = null
+      if(this.model.children.length > 0) {
+        lastChild=this.model.children[this.model.children.length - 1]
+      }
+      if (
+        this.model.children.length == 0 ||
+        (lastChild &&
+          (lastChild.flavour != 'affine:paragraph' ||  (lastChild.flavour == 'affine:paragraph' && lastChild.type && lastChild.type=='quote' )))
+      ) {
+        this.doc.addBlock('affine:paragraph', {}, this.model);
+      }
+    }catch (e) {
+      console.log("error",e);
+    }finally {
+
+    }*/
+    return result;
+  }
+
   override connectedCallback() {
     super.connectedCallback();
+
     this.disposables.add(
       effect(() => {
         checkNotEmptyNote(this.model, this.doc);
