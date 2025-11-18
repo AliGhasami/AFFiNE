3143c027bc157c276af858d55b9a7f0111eddf8e
alighasami <alighassami@yahoo.com>
2025-11-18 09:37:35 +0330

    fix: show error file size and check and prevent drop bad image file in editor

--- blocksuite/affine/blocks/mahdaad-object/src/mahdaad-object-service.ts
+++ blocksuite/affine/blocks/mahdaad-object/src/mahdaad-object-service.ts
@@ -3,15 +3,34 @@ import { FileDropConfigExtension } from '@blocksuite/affine-components/drop-indi
 import { MahdaadObjectBlockSchema } from '@blocksuite/affine-model';
 import { matchModels } from '@blocksuite/affine-shared/utils';
 import { addSiblingObjectBlock } from './utils';
+import {
+  checkFileFormat,
+  checkFileSize,
+} from '../../../../../../src/utils/fileUploadUtils';
 
 export const ImageDropOption = FileDropConfigExtension({
   flavour: MahdaadObjectBlockSchema.model.flavour,
-  onDrop: ({ files, targetModel, std }) => {
-    const maxFileSize = 10 * 1000 * 1000; // 10MB (default)
-    const imageFiles = files.filter(file =>
-      ['image/jpeg', 'image/png'].includes(file.type)
-    );
-    if (!imageFiles.length) return false;
+  onDrop: async ({ files, targetModel, std }) => {
+    const maxFileSize = import.meta.env.VITE_IMAGE_MAX_UPLOAD_SIZE; // 10MB (default)
+    /*const imageFiles = files.filter(async file => {
+      const res = await checkFileFormat(file, 'image');
+      return (
+        res && checkFileSize(file, import.meta.env.VITE_IMAGE_MAX_UPLOAD_SIZE)
+      ); //['image/jpeg', 'image/png'].includes(file.type);
+    });
+    if (!imageFiles.length) return false;*/
+    const imageFiles = [];
+    for (const file of files) {
+      const formatOk = await checkFileFormat(file, 'image');
+      const sizeOk = checkFileSize(
+        file,
+        import.meta.env.VITE_IMAGE_MAX_UPLOAD_SIZE
+      );
+
+      if (formatOk && sizeOk) {
+        imageFiles.push(file);
+      }
+    }
 
     if (targetModel && !matchModels(targetModel, [SurfaceBlockModel])) {
       addSiblingObjectBlock(
